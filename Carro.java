70866cb86322b497f0d9036d701312bbfb74aeba
Mateo Enriquez <mateo.enriquez@udla.edu.ec>
2024-06-10 17:36:32 -0500

    Constructores y metodos en clase Carro

--- Carro.java
+++ Carro.java
@@ -8,7 +8,44 @@ public class Carro
     //Hora dellegada del carro al parqueadero: valor entre 6 y 20
     private int horaLlegada;
 
+    //Constructores
 
+    public Carro (String pPlaca, int pHora)
+    {
+        placa = pPlaca;
+        horaLlegada = pHora;
+    }
 
+    //Metodos
+
+    public String darPlaca()
+    {
+        return placa;
+    }
+
+    public int darHoraLLegada()
+    {
+        return horaLlegada;
+    }
+
+    public boolean tienePlaca(String pPlaca)
+    {
+        boolean tienePLaca = false;
+
+        if (placa.equalsIgnoreCase(pPlaca))
+        {
+            tienePLaca = true;
+        }
+        else
+        {
+            tienePLaca = false;
+        }
+        return tienePLaca;
+    }
+    public int darTiempoEnParqueadero(int pHoraSalida)
+    {
+        int tiempoParqueadero = pHoraSalida - horaLlegada + 1;
+        return tiempoParqueadero;
+    }
 
 }
