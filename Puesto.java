cfc26867cd5e3bfdda98dd1af0887f1ac7d62c0a
Mateo Enriquez <mateo.enriquez@udla.edu.ec>
2024-06-10 18:06:01 -0500

    Metodos en clase Puesto

--- Puesto.java
+++ Puesto.java
@@ -16,5 +16,49 @@ public class Puesto
 
     //Metodos
 
+    public Carro darCarro()
+    {
+        return carro;
+    }
+    public boolean estaOcupado()
+    {
+        boolean ocupado = carro != null;
+        return ocupado;
+    }
+
+    public void parquearCarro(Carro pCarro)
+    {
+        carro = pCarro;
+    }
+
+    public void sacarCarro()
+    {
+        carro = null;
+    }
+
+    public int darNumeroPuesto()
+    {
+        return numeroPuestos;
+    }
+
+    public boolean tieneCarroConPlaca(String pPlaca)
+    {
+        boolean tieneCarro = true;
+
+        if (carro == null)
+        {
+            tieneCarro = false;
+        } else if (carro.tienePlaca(pPlaca))
+        {
+         tieneCarro = true;
+        }
+        else
+        {
+            tieneCarro = false;
+        }
+
+        return tieneCarro;
+    }
+
 
 }
