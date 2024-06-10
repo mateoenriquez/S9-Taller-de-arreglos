1f8b6b91ddadb6ad898fdec4a240bd41ca896a81
Mateo Enriquez <mateo.enriquez@udla.edu.ec>
2024-06-10 17:45:45 -0500

    Constantes en clase Parqueadero

---
+++ Parqueadero.java
@@ -0,0 +1,26 @@
+public class Parqueadero
+{
+    //Constantes
+
+    //Numero de puestos en el parqueadero
+    public static final int TAMANO = 40;
+
+    //Cuando el parqueadero esta lleno
+    public static final int NO_HAY_PUESTO = -1;
+
+    //Cuando el parqueadero esta cerrado
+    public static final int PARQUEADERO_CERRADO = -2;
+
+    //Cuando el carro que se busca no esta dentro del parqueadero
+    public static final int CARRO_NO_EXISTE = -3;
+
+    //Hora a la que abre el parqueadero
+    public static final int HORA_INICIAL = 6;
+
+    //Hora de cierre del parqueadero
+    public static final int HORA_CIERRE = 20;
+
+    //La tarifa inicial del parqueadero
+    public static final int TARIFA_INICIAL = 1200;
+
+}
