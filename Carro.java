5abc3d1daf9eb5879bb4a027a155e04305c4a0b6
Mateo Enriquez <mateo.enriquez@udla.edu.ec>
2024-06-10 17:01:49 -0500

    Atributos en clase carro

---
+++ Carro.java
@@ -0,0 +1,14 @@
+public class Carro
+{
+    //Atributos
+
+    //PLaca del carro
+    private String placa;
+
+    //Hora dellegada del carro al parqueadero: valor entre 6 y 20
+    private int horaLlegada;
+
+
+
+
+}
