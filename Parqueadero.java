4fe882b4cd7b9600d7321c52d77bd35b55fd8e67
Mateo Enriquez <mateo.enriquez@udla.edu.ec>
2024-06-10 21:19:34 -0500

    Metodos en clase Parqueadero

--- Parqueadero.java
+++ Parqueadero.java
@@ -50,10 +50,105 @@ public class Parqueadero
         }
     }
 
+    //Metodos
+
     public String darPlacaCarro(int pPosicion)
     {
         String respuesta = "";
-        if (estaOcupado())
+        if (estaOcupado(pPosicion))
+        {
+            respuesta = "Placa: " + puestos[pPosicion].darCarro().darPlaca();
+        }
+        else
+        {
+            respuesta = "No hay un carro en esta posicion";
+        }
+
+        return respuesta;
+    }
+
+    public int entrarCarro(String pPlaca)
+    {
+        int resultado = 0;
+        if(!abierto)
+        {
+            resultado = PARQUEADERO_CERRADO;
+        }
+        else
+        {
+            //Buscar un puesto libre para el carro y agregarlo
+            int numPuestoCarro = buscarPuestoCaroo(pPlaca.toUpperCase());
+            if (numPuestoCarro != CARRO_NO_EXISTE)
+            {
+                resultado = CARRO_NO_EXISTE;
+            }
+
+            //Busco un puesto libre para el carro y agregarlo
+            resultado = buscarPuestoLibre();
+            if (resultado != NO_HAY_PUESTO)
+            {
+                Carro carroEntrando = new Carro(pPlaca, horaActual);
+                puestos[resultado].parquearCarro(carroEntrando);
+            }
+        }
+
+        return resultado;
+    }
+
+    public int sacarCarro(String pPlaca)
+    {
+        int resultado = 0;
+        if (!abierto)
+        {
+            resultado = PARQUEADERO_CERRADO;
+        }
+        else
+        {
+            int numPuesto = buscarPuestoCarro(pPlaca.toUpperCase());
+            if (numPuesto == CARRO_NO_EXISTE)
+            {
+                resultado = CARRO_NO_EXISTE;
+            }
+            else
+            {
+                Carro carro = puestos[numPuesto].darCarro();
+                int nHoras = carro.darTiempoEnParqueadero(horaActual);
+                int porPagar = nHoras * tarifa;
+                caja = caja + porPagar;
+                puestos[numPuesto].sacarCarro();
+                resultado = porPagar;
+            }
+        }
+
+        return resultado;
+    }
+
+    public int darMontoCaja()
+    {
+        return caja;
+    }
+
+    public int calcularPuestosLibres()
+    {
+        int puestosLibres = 0;
+        for (Puesto puesto : puestos)
+        {
+            if (!puesto.estaOcupado())
+            {
+                puestosLibres = puestosLibres + 1;
+            }
+        }
+        return puestosLibres;
+    }
+
+    public void cambiarTarifca(int pTarifa)
+    {
+        tarifa = pTarifa;
+    }
+
+    private int buscarPuestoLibre()
+    {
+        int puesto = NO_HAY_PUESTO;
     }
 
 
